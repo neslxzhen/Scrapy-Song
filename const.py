d49a728aaec460bf8cc26880d32742a59df5d967
neslxzhen <neslxzhen@gmail.com>
2020-03-22 16:26:54 +0800

    rebuild

---
+++ const.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+import os
+import re
+import sys
+import locale
+from datetime import datetime
+import requests
+
+# info
+DEVELOPMENT = False
+__version__ = 0.2
+PROJECT_NAME="OPED-Getter"
+
+# File Name
+date=datetime.now().strftime('%Y%m%d%H%M%S')
+LOG_FILE_PATH='./log/'
+LOG_FILE_NAME=LOG_FILE_PATH+date+"_log.txt"
+IN_FILE_NAME='in.txt'
+
+if not os.path.isdir(LOG_FILE_PATH): os.mkdir(LOG_FILE_PATH)
+
+# thread
+THREAD_CNT=30
+WAIT_OTHER_THREAD=True
+
+# var
+RETRY_CNT=3
+RETRY_DELAY=10
